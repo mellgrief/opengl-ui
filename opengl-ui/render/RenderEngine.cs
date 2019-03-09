f6bbeef3326031be09f759bc8e8cd8553141d883
mellgrief <dedhost@yandex.ru>
2019-03-09 13:16:04 +0300

    start

---
+++ opengl-ui/render/RenderEngine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using opengl_ui.render.element;
+using opengl_ui.render.impl;
+using OpenTK;
+using OpenTK.Graphics;
+
+namespace opengl_ui.render
+{
+    class RenderEngine : GameWindow
+    {
+
+        public static IScene currentScene;
+        public static List<Element> elements = new List<Element>();
+
+        public RenderEngine() : base(800, 800, GraphicsMode.Default, "ZMEYA")
+        {
+
+        }
+        
+        public static void setScene(IScene sc)
+        {
+            currentScene = sc;
+        }
+
+    }
+}
---
+++ opengl-ui/render/impl/IScene.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opengl_ui.render.impl
+{
+    interface IScene
+    {
+        
+    }
+}
